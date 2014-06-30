595a0687bcc02ee2045b6a9b8a1d9d1c08360a65
JJ Allaire <jj@rstudio.org>
2014-06-29 22:38:57 -0400

    parallel inner product example

---
+++ inst/examples/parallel-inner-product.cpp
@@ -0,0 +1,61 @@
+#include <Rcpp.h>
+using namespace Rcpp;
+
+#include <algorithm>
+
+// [[Rcpp::depends(TBB)]]
+#include <tbb/tbb.h>
+
+struct InnerProductBody {
+   
+   // source vectors
+   double * const x;
+   double * const y;
+   
+   // sum that I have accumulated
+   double product;
+   
+   // standard and splitting constructor  
+   InnerProductBody(double * const x, double * const y) 
+      : x(x), y(y), product(0) {}
+   InnerProductBody(InnerProductBody& body, tbb::split) 
+      : x(body.x), y(body.y), product(0) {}
+   
+   // accumulate just the element of the range I've been asked to
+   void operator()(const tbb::blocked_range<size_t>& r) {
+      product += std::inner_product(x + r.begin(), 
+                                    x + r.end(), 
+                                    y + r.begin(), 0.0);
+   }
+   
+   // join my inner product with another one
+   void join(InnerProductBody& rhs) { product += rhs.product; }
+};
+
+// [[Rcpp::export]]
+double parallelInnerProduct(NumericVector x, NumericVector y) {
+   
+   // declare the SumBody instance that takes a pointer to the vector data
+   InnerProductBody innerProductBody(x.begin(), y.begin());
+   
+   // call parallel_reduce to start the work
+   tbb::parallel_reduce(tbb::blocked_range<size_t>(0, x.length()), 
+                        innerProductBody);
+   
+   // return the computed sum
+   return innerProductBody.product;
+}
+
+/*** R
+ 
+x <- runif(1000000)
+y <- runif(1000000)
+
+library(rbenchmark)
+res <- benchmark(sum(x*y),
+                 parallelInnerProduct(x, y),
+                 order="relative")
+res[,1:4]
+*/
+
+
