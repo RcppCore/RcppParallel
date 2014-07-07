fe26cae577c0236d4179e0c1ba781d72e462376d
JJ Allaire <jj@rstudio.org>
2014-07-07 15:46:40 -0400

    indent 3 spaces

--- inst/include/RcppParallel.h
+++ inst/include/RcppParallel.h
@@ -20,8 +20,8 @@ namespace RcppParallel {
 // cast the void* to within the worker thread.
 
 struct Worker {
-  virtual ~Worker() {}
-  virtual void operator()(std::size_t begin, std::size_t end) = 0;
+   virtual ~Worker() {}
+   virtual void operator()(std::size_t begin, std::size_t end) = 0;
 };
 
 } // namespace RcppParallel
@@ -45,19 +45,19 @@ namespace {
 class IndexRange {
 public:
 
-  // Initizlize with a begin and (exclusive) end index
-  IndexRange(std::size_t begin, std::size_t end)
+   // Initizlize with a begin and (exclusive) end index
+   IndexRange(std::size_t begin, std::size_t end)
     : begin_(begin), end_(end)
-  {
-  }
+   {
+   }
   
-  // Access begin() and end()
-  std::size_t begin() const { return begin_; }
-  std::size_t end() const { return end_; }
+   // Access begin() and end()
+   std::size_t begin() const { return begin_; }
+   std::size_t end() const { return end_; }
   
 private:
-  std::size_t begin_;
-  std::size_t end_;
+   std::size_t begin_;
+   std::size_t end_;
 };
 
 
@@ -65,114 +65,114 @@ private:
 // we need to pass our worker and range within a struct that we 
 // can cast to/from void*
 struct Work {
-  Work(IndexRange range, Worker& worker) 
+   Work(IndexRange range, Worker& worker) 
     :  range(range), worker(worker)
-  {
-  }
-  IndexRange range;
-  Worker& worker;
+   {
+   }
+   IndexRange range;
+   Worker& worker;
 };
 
 // Thread which performs work (then deletes the work object
 // when it's done)
 extern "C" inline void workerThread(void* data) {
-  try
-  {
-    Work* pWork = static_cast<Work*>(data);
-    pWork->worker(pWork->range.begin(), pWork->range.end());
-    delete pWork;
-  }
-  catch(...)
-  {
-  }
+   try
+   {
+      Work* pWork = static_cast<Work*>(data);
+      pWork->worker(pWork->range.begin(), pWork->range.end());
+      delete pWork;
+   }
+   catch(...)
+   {
+   }
 }
 
 // Function to calculate the ranges for a given input
 std::vector<IndexRange> splitInputRange(const IndexRange& range) {
   
-  // determine number of threads
-  std::size_t threads = tthread::thread::hardware_concurrency();
-  char* numThreads = ::getenv("RCPP_PARALLEL_NUM_THREADS");
-  if (numThreads != NULL) {
-     int parsedThreads = ::atoi(numThreads);
-     if (parsedThreads > 0)
-        threads = parsedThreads;
-  }
+   // determine number of threads
+   std::size_t threads = tthread::thread::hardware_concurrency();
+   char* numThreads = ::getenv("RCPP_PARALLEL_NUM_THREADS");
+   if (numThreads != NULL) {
+      int parsedThreads = ::atoi(numThreads);
+      if (parsedThreads > 0)
+         threads = parsedThreads;
+   }
        
-  // determine the chunk size
-  std::size_t length = range.end() - range.begin();
-  std::size_t chunkSize = length / threads;
+   // determine the chunk size
+   std::size_t length = range.end() - range.begin();
+   std::size_t chunkSize = length / threads;
   
-  // allocate ranges
-  std::vector<IndexRange> ranges;
-  std::size_t nextIndex = range.begin();
-  for (std::size_t i = 0; i<threads; i++) {
-    std::size_t begin = nextIndex;
-    std::size_t end = std::min(begin + chunkSize, range.end());
-    ranges.push_back(IndexRange(begin, end));
-    nextIndex = end;
-  }
-
-  // return ranges  
-  return ranges;
+   // allocate ranges
+   std::vector<IndexRange> ranges;
+   std::size_t nextIndex = range.begin();
+   for (std::size_t i = 0; i<threads; i++) {
+      std::size_t begin = nextIndex;
+      std::size_t end = std::min(begin + chunkSize, range.end());
+      ranges.push_back(IndexRange(begin, end));
+      nextIndex = end;
+   }
+   
+   // return ranges  
+   return ranges;
 }
 
 } // anonymous namespace
 
-// Execute the IWorker over the IndexRange in parallel
+// Execute the Worker over the IndexRange in parallel
 inline void ttParallelFor(std::size_t begin, std::size_t end, Worker& worker) {
   
-  using namespace tthread;
-  
-  // split the work
-  std::vector<IndexRange> ranges = splitInputRange(IndexRange(begin, end));
-  
-  // create threads
-  std::vector<thread*> threads;
-  for (std::size_t i = 0; i<ranges.size(); ++i) {
-    threads.push_back(new thread(workerThread, new Work(ranges[i], worker)));   
-  }
-  
-  // join and delete them
-  for (std::size_t i = 0; i<threads.size(); ++i) {
-    threads[i]->join();
-    delete threads[i];
-  }
+   using namespace tthread;
+   
+   // split the work
+   std::vector<IndexRange> ranges = splitInputRange(IndexRange(begin, end));
+   
+   // create threads
+   std::vector<thread*> threads;
+   for (std::size_t i = 0; i<ranges.size(); ++i) {
+      threads.push_back(new thread(workerThread, new Work(ranges[i], worker)));   
+   }
+   
+   // join and delete them
+   for (std::size_t i = 0; i<threads.size(); ++i) {
+      threads[i]->join();
+      delete threads[i];
+   }
 }
 
 // Execute the IWorker over the range in parallel then join results
 template <typename Reducer>
 inline void ttParallelReduce(std::size_t begin, std::size_t end, Reducer& reducer) {
   
-  using namespace tthread;
-  
-  // split the work
-  std::vector<IndexRange> ranges = splitInputRange(IndexRange(begin, end));
-  
-  // create threads (split for each thread and track the allocated workers)
-  std::vector<thread*> threads;
-  std::vector<Worker*> workers;
-  for (std::size_t i = 0; i<ranges.size(); ++i) {
-    Reducer* pReducer = new Reducer();
-    pReducer->split(static_cast<Reducer&>(reducer));
-    workers.push_back(pReducer);
-    threads.push_back(new thread(workerThread, new Work(ranges[i], *pReducer)));  
-  }
-  
-  // wait for each thread, join it's results, then delete the worker & thread
-  for (std::size_t i = 0; i<threads.size(); ++i) {
-    
-    // wait for thread
-    threads[i]->join();
+   using namespace tthread;
    
-    // join the results
-    reducer.join(static_cast<Reducer&>(*workers[i]));
-    
-    // delete the worker (which we split above) and the thread
-    delete workers[i];
-    delete threads[i];
-  }
+   // split the work
+   std::vector<IndexRange> ranges = splitInputRange(IndexRange(begin, end));
+   
+   // create threads (split for each thread and track the allocated workers)
+   std::vector<thread*> threads;
+   std::vector<Worker*> workers;
+   for (std::size_t i = 0; i<ranges.size(); ++i) {
+      Reducer* pReducer = new Reducer();
+      pReducer->split(static_cast<Reducer&>(reducer));
+      workers.push_back(pReducer);
+      threads.push_back(new thread(workerThread, new Work(ranges[i], *pReducer)));  
+   }
+   
+   // wait for each thread, join it's results, then delete the worker & thread
+   for (std::size_t i = 0; i<threads.size(); ++i) {
+      // wait for thread
+      threads[i]->join();
+      
+      // join the results
+      reducer.join(static_cast<Reducer&>(*workers[i]));
+      
+      // delete the worker (which we split above) and the thread
+      delete workers[i];
+      delete threads[i];
+   }
 }
+
 } // namespace RcppParallel
 
 ////// TBB Implementation /////////////////////////////////////////////
@@ -185,14 +185,14 @@ namespace RcppParallel {
 
 struct TBBWorker
 {
-  explicit TBBWorker(Worker& worker) : worker_(worker) {}
-  
-  void operator()(const tbb::blocked_range<size_t>& r) const {
-    worker_(r.begin(), r.end());
-  }
-  
+   explicit TBBWorker(Worker& worker) : worker_(worker) {}
+   
+   void operator()(const tbb::blocked_range<size_t>& r) const {
+      worker_(r.begin(), r.end());
+   }
+
 private:
-  Worker& worker_;
+   Worker& worker_;
 };
 
 inline void tbbParallelFor(std::size_t begin, std::size_t end, Worker& worker) {
@@ -203,35 +203,35 @@ inline void tbbParallelFor(std::size_t begin, std::size_t end, Worker& worker) {
 template <typename Reducer>
 struct TBBReducer 
 {  
-  explicit TBBReducer(Reducer& reducer) 
+   explicit TBBReducer(Reducer& reducer) 
     : pReducer_(&reducer), wasSplit_(false)
-  {
-  }
+   {
+   }
    
-  virtual ~TBBReducer() {
-    try
-    {
-      if (wasSplit_)
-        delete pReducer_;
-    }
-    catch(...)
-    {
-    }
-  }
+   virtual ~TBBReducer() {
+      try
+      {
+         if (wasSplit_)
+            delete pReducer_;
+      }
+      catch(...)
+      {
+      }
+   }
    
-  void operator()(const tbb::blocked_range<size_t>& r) {
-    pReducer_->operator()(r.begin(), r.end());
-  }
+   void operator()(const tbb::blocked_range<size_t>& r) {
+      pReducer_->operator()(r.begin(), r.end());
+   }
    
-  TBBReducer(TBBReducer& reducer, tbb::split) 
-    : pReducer_(new Reducer()), wasSplit_(true) 
-  {  
-    pReducer_->split(*reducer.pReducer_);   
-  }
-  
-  void join(const TBBReducer& reducer) { 
-    pReducer_->join(*reducer.pReducer_); 
-  }
+   TBBReducer(TBBReducer& reducer, tbb::split) 
+      : pReducer_(new Reducer()), wasSplit_(true) 
+   {  
+      pReducer_->split(*reducer.pReducer_);   
+   }
+
+   void join(const TBBReducer& reducer) { 
+      pReducer_->join(*reducer.pReducer_); 
+   }
    
 private:
    Reducer* pReducer_;
@@ -240,7 +240,6 @@ private:
 
 template <typename Reducer>
 inline void tbbParallelReduce(std::size_t begin, std::size_t end, Reducer& reducer) {
-    
    TBBReducer<Reducer> tbbReducer(reducer);
    tbb::parallel_reduce(tbb::blocked_range<size_t>(begin, end), tbbReducer);
 }
@@ -256,9 +255,9 @@ namespace RcppParallel {
 inline void parallelFor(std::size_t begin, std::size_t end, Worker& worker) {
   
 #if RCPP_PARALLEL_USE_TBB
-  tbbParallelFor(begin, end, worker);
+   tbbParallelFor(begin, end, worker);
 #else
-  ttParallelFor(begin, end, worker);
+   ttParallelFor(begin, end, worker);
 #endif
 
 }
@@ -267,9 +266,9 @@ template <typename Reducer>
 inline void parallelReduce(std::size_t begin, std::size_t end, Reducer& reducer) {
 
 #if RCPP_PARALLEL_USE_TBB
-  tbbParallelReduce(begin, end, reducer);
+   tbbParallelReduce(begin, end, reducer);
 #else
-  ttParallelReduce(begin, end, reducer);
+   ttParallelReduce(begin, end, reducer);
 #endif
 
 }
