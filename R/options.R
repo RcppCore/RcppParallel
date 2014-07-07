f4e34fd27a2ea55716f3ded41e8f79701a24a8e0
JJ Allaire <jj@rstudio.org>
2014-07-07 13:47:04 -0400

    link on windows

--- R/options.R
+++ R/options.R
@@ -3,19 +3,19 @@
 dllInfo <- NULL
 
 .onLoad <- function(libname, pkgname) {
-   
-   # load tbb and make it's symbols available globally
+
+   # load tbb if we aren't on windows
    sysname <- Sys.info()['sysname']
-   if (sysname == "Darwin")
-      ext = ".dylib"
-   else if (sysname == "Linux")
-      ext = ".so.2"
-   else if (sysname == "Windows")
-      ext = ".dll"
-   else
-      ext = .Platform$dynlib.ext
-   dll <- system.file(paste("lib/libtbb", ext, sep = ""), package = "RcppParallel")
-   dllInfo <<- dyn.load(dll, local = FALSE, now = TRUE)
+   if (sysname != "Windows") {
+     if (sysname == "Darwin")
+        ext = ".dylib"
+     else if (sysname == "Linux")
+        ext = ".so.2"
+     else
+        ext = .Platform$dynlib.ext
+     dll <- system.file(paste("lib/libtbb", ext, sep = ""), package = "RcppParallel")
+     dllInfo <<- dyn.load(dll, local = FALSE, now = TRUE)
+   }
    
    # load the package library
    library.dynam("RcppParallel", pkgname, libname)
@@ -29,8 +29,9 @@ dllInfo <- NULL
    # unload the package library
    library.dynam.unload("RcppParallel", libpath)
    
-   # unload tbb
-   dyn.unload(dllInfo[["path"]])
+   # unload tbb if we loaded it (i.e. aren't on windows)
+   if (!is.null(dllInfo))
+      dyn.unload(dllInfo[["path"]])
 }
 
 setThreadOptions <- function(numThreads = "auto", stackSize = "auto") {
