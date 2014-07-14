7c8c02bad9e128da4524f8aa3ae2181d63216fcb
JJ Allaire <jj@rstudio.org>
2014-07-14 10:03:15 -0400

    parallel version

--- inst/examples/parallel-distance-matrix.cpp
+++ inst/examples/parallel-distance-matrix.cpp
@@ -2,12 +2,11 @@
 #include <Rcpp.h>
 using namespace Rcpp;
 
-#include <iostream>
-
 // [[Rcpp::depends(RcppParallel)]]
 #include <RcppParallel.h>
 using namespace RcppParallel;
 
+#include <cmath>
 
 // [[Rcpp::export]]
 double kl_divergence(NumericVector vec1, NumericVector vec2) {
@@ -41,48 +40,84 @@ NumericMatrix js_distance(NumericMatrix mat) {
   return rmat;
 }
 
-
-// [[Rcpp::export]]
-void testMatrix(NumericMatrix x) {
+struct JsDistance : public Worker {
    
-   RMatrix<double> m(x);
-    
-   //NumericMatrixAccessor::Column column = m.column(3);
+   RMatrix<double> input;
+   RMatrix<double> output;
    
-   for (RMatrix<double>::Column::iterator it = m.column(3).begin();
-        it != m.column(3).end(); ++it) {
-      std::cout << *it << std::endl;  
-   }
+   JsDistance(NumericMatrix input, NumericMatrix output)
+      : input(input), output(output) {}
    
-   
-   //NumericMatrixAccessor::Row row = m.row(3);
-   
-   for (RMatrix<double>::Row::iterator it = m.row(3).begin();
-        it != m.row(3).end(); ++it) {
-      std::cout << *it << std::endl;  
-   }
-   
-   for (size_t i = 0; i<m.ncol(); i++) {
-      for (size_t j = 0; j<m.nrow(); j++) {
-         std::cout << m(j, i) << std::endl;
+   void operator()(std::size_t begin, std::size_t end) {
+      
+      for (int i = begin; i < end; i++) {
+         
+         for (int j = 0; j < i; j++) {
+         
+            RMatrix<double>::Row row1 = input.row(i);
+            RMatrix<double>::Row row2 = input.row(j);
+            
+            std::vector<double> avg(row1.length());
+            std::transform(row1.begin(), row1.end(), row2.begin(),
+                           avg.begin(), average);
+              
+            double d1 = divergence(row1, avg);
+            double d2 = divergence(row2, avg);
+               
+            output(i,j) = sqrt(.5 * (d1 + d2));
+         }
       }
    }
    
+   double divergence(RMatrix<double>::Row row, const std::vector<double>& avg) {
+      double rval = 0;
+      for (int i = 0; i < row.length(); i++) {
+         double r = 0;
+         if (row[i] > 0 && avg[i] > 0)
+            r = log(row[i]/avg[i]) * row[i];
+         rval += r;
+      }
+      return rval;  
+   }
    
-}
+   static double average(double val1, double val2) {
+      return (val1 + val2) / 2;
+   }
+};
 
+
+// [[Rcpp::export]]
+NumericMatrix parallel_js_distance(NumericMatrix mat) {
+  
+   // allocate the matrix we will return
+   NumericMatrix rmat(mat.nrow(), mat.nrow());
+
+   // create the worker
+   JsDistance jsDistance(mat, rmat);
+   
+   jsDistance(0, mat.nrow());
+   
+   // call it in parallel
+   //parallelFor(0, mat.nrow(), jsDistance);
+
+   return rmat;
+}
 
 
 /*** R
 
+n  = 1000
+m = matrix(runif(n*10), ncol = 10)
+m = m/rowSums(m)
+mt = t(m)
+all.equal(js_distance(m), parallel_js_distance(m)) 
 
-m = matrix(as.numeric(c(1:16)), nrow = 4, ncol = 4)
-testMatrix(m)
-
-#n  = 1000
-#m = matrix(runif(n*10), ncol = 10)
-#m = m/rowSums(m)
-#js_distance(m) 
+library(rbenchmark)
+res <- benchmark(js_distance(m),
+                 parallel_js_distance(m),
+                 replications = 2,
+                 order="relative")
+res[,1:4]
 */
 
 
