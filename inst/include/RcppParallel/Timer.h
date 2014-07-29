cb818ab16e40303b6d9bfd80472f76b806ba1d27
Romain François <romain@r-enthusiasts.com>
2014-07-29 19:36:46 +0200

    added some capabilities to ProportionTimer, and refine TimesList to track data size handled by each worker

--- inst/include/RcppParallel/Timer.h
+++ inst/include/RcppParallel/Timer.h
@@ -7,9 +7,13 @@ namespace RcppParallel {
     template <typename Timer>
     class ProportionTimer {
     public:
-        ProportionTimer() : timer(), n(0){}
+        ProportionTimer() : 
+            timer(), n(0), id(0)
+        {}
         
-        ProportionTimer( nanotime_t origin ) : timer(origin), n(0){}
+        ProportionTimer( nanotime_t origin, int id_ ) : 
+            timer(origin), n(0), id(id_)
+        {}
         
         inline operator SEXP() const {
             Rcpp::NumericVector out = (SEXP)timer ;
@@ -31,6 +35,8 @@ namespace RcppParallel {
         
         Timer timer ;
         int n ;
+        int id ;
+        
     } ;
     
     template <typename Timer>
@@ -67,7 +73,6 @@ namespace RcppParallel {
             return out ;
         }
         
-        
     private:
         std::vector<Timer> timers ;
     } ;
@@ -76,35 +81,54 @@ namespace RcppParallel {
     class TimersList {
     public:
         
-        TimersList(): timers(){}
+        TimersList(): timers(){
+            timers.push_back( ProportionTimer<Timer>() );
+            childs.push_back( std::vector<int>() ) ;
+        }
         
-        Timer& get_new_timer(){
+        ProportionTimer<Timer>& get_new_timer(int parent_id){
             Locker lock(mutex) ;
-            if( timers.empty() ) {
-                timers.push_back( Timer() ) ;
-            } else {
-                timers.push_back( Timer( timers.front().origin() ) ) ;
-            }
+            int id = timers.size() ;
+            timers.push_back( ProportionTimer<Timer>( timers.front().origin(), id ) ) ;
+            childs.push_back( std::vector<int>() );
+            std::list<std::vector<int> >::iterator it = childs.begin() ;
+            for(int i=0; i<parent_id; i++) ++it ;
+            it->push_back(id) ;
             return timers.back() ;
         }
         
         inline operator SEXP(){
-            Rcpp::List data = wrap(timers) ;
+            int n = timers.size() ;
+            Rcpp::List data(n) ;
+            typename std::list<ProportionTimer<Timer> >::const_iterator timers_it = timers.begin() ;
+            std::list<std::vector<int> >::const_iterator childs_it = childs.begin() ;
+            for( int i=0; i<n; i++, ++timers_it, ++childs_it ){
+                Rcpp::NumericVector x = (SEXP)*timers_it ;
+                x.attr("childs") = Rcpp::wrap( *childs_it ) ;
+                x.attr("id") = timers_it->id ;
+                data[i] = x ;
+            }
             data.attr("class") = Rcpp::CharacterVector::create( "TimersList" , "Timer" ) ;
             return data ;
         }
         
-    private:
-        TimersList( const TimersList& ) ;
-        std::list<Timer> timers ;
+        inline ProportionTimer<Timer>& front(){ 
+            return timers.front(); 
+        }
+        
+        std::list<ProportionTimer<Timer> > timers ;
+        std::list<std::vector<int> > childs ;
         Mutex mutex ;
         
+    private:
+        TimersList( const TimersList& ) ;
+    
     } ;
     
     template <typename Reducer, typename Timer, typename Mutex, typename Locker>
     class TimedReducer : public Worker {
     public:
-        typedef TimersList<ProportionTimer<Timer>,Mutex,Locker> Timers ; 
+        typedef TimersList<Timer,Mutex,Locker> Timers ; 
         
         Reducer* reducer ;
         Timers& timers ;
@@ -114,7 +138,7 @@ namespace RcppParallel {
         TimedReducer( Reducer& reducer_, Timers& timers_) : 
             reducer(&reducer_), 
             timers(timers_), 
-            timer(timers.get_new_timer()), 
+            timer(timers.get_new_timer(0)), 
             owner(false)
         {
             timer.step("init");
@@ -123,14 +147,14 @@ namespace RcppParallel {
         TimedReducer( const TimedReducer& other, Split s) : 
             reducer( new Reducer(*other.reducer, s) ), 
             timers( other.timers ), 
-            timer( timers.get_new_timer() ), 
+            timer( timers.get_new_timer(other.timer.id) ),
             owner(true)
         {
             timer.step("init") ;
         }
         
         ~TimedReducer(){
-            if(owner) {
+            if(owner && reducer) {
                 delete reducer ;
             }
             reducer = 0 ;
@@ -139,14 +163,14 @@ namespace RcppParallel {
         inline void operator()( size_t begin, size_t end){
             timer.n += (end-begin) ;
             timer.step("start") ;
-            (*reducer)(begin, end) ;
+            reducer->operator()(begin, end) ;
             timer.step("work") ;
         }
         
         inline void join(const TimedReducer& rhs){
             timer.n += rhs.timer.n ;
             timer.step("start") ;
-            (*reducer).join(*rhs.reducer) ;
+            reducer->join(*rhs.reducer) ;
             timer.step("join") ;
         }
         
