89533c74ec8a32b87693c52a84f7f9088c546c99
Kevin Ushey <kevinushey@gmail.com>
2016-01-14 15:19:04 -0800

    example using fast_sqrt

--- examples/rcpp-simd-transform.cpp
+++ examples/rcpp-simd-transform.cpp
@@ -43,9 +43,31 @@ double simd_sum(NumericVector data)
    return simd::accumulate(data, 0.0, plus());
 }
 
+#include <boost/simd/arithmetic/include/functions/fast_sqrt.hpp>
+
+// NOTE: To use the bundled SIMD functions, it looks like we need the
+// following header. We should expose all of these functions and auto-include
+// them in a utility namespace so users don't have to keep track of this sort
+// of thing.
+#include <boost/dispatch/functor/meta/call.hpp>
+
+// It also stinks that we have to wrap it in our own functor. Surely
+// there's a better way?
+struct fast_sqrt {
+   template <typename T>
+   T operator()(const T& t) { return boost::simd::fast_sqrt(t); }
+};
+
+// [[Rcpp::export]]
+NumericVector simd_sqrt(NumericVector data)
+{
+   return simd::transform(data, fast_sqrt());
+}
+
 /*** R
 data <- as.numeric(1:16)
 simd_add_two(data)
 simd_add(data, data)
 simd_sum(data)
+simd_sqrt(data)
 */
