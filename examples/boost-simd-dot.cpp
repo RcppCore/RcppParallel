13fd5dc7854dc48ccb1c3959c5c1a79a586275f3
Kevin Ushey <kevinushey@gmail.com>
2016-01-15 16:40:15 -0800

    update / fix 'dot' example

--- examples/boost-simd-dot.cpp
+++ examples/boost-simd-dot.cpp
@@ -12,58 +12,49 @@ using namespace Rcpp;
 #include <boost/simd/include/functions/multiplies.hpp>
 
 template <typename Value>
-Value simd_dot(Value* first1, Value* last1, Value* first2)
+Value simd_dot_impl(Value* first1, Value* last1, Value* first2)
 {
    using boost::simd::sum;
    using boost::simd::pack;
    
-   typedef pack<Value> type;
-   type tmp;
+   typedef pack<Value> packed_type;
+   packed_type tmp;
    
    // Let's consider that (last1-first1) is divisible by the size of the pack.
    while (first1 != last1)
    {
       // Load current values from the datasets
-      pack<Value> x1 = boost::simd::aligned_load<Value>(first1);
-      pack<Value> x2 = boost::simd::aligned_load<Value>(first2);
+      packed_type x1 = boost::simd::load<packed_type>(first1);
+      packed_type x2 = boost::simd::load<packed_type>(first2);
       
       // Computation
       tmp = tmp + x1 * x2;
       
       // Advance to the next SIMD vector
-      first1 += type::static_size;
-      first2 += type::static_size;
+      first1 += packed_type::static_size;
+      first2 += packed_type::static_size;
    }
    
    return sum(tmp);
 }
 
 // [[Rcpp::export]]
-double dot(NumericVector lhs, NumericVector rhs)
+double simd_dot(NumericVector lhs, NumericVector rhs)
 {
-   // construct simd vectors
-   typedef std::vector< double, boost::simd::allocator<double> > vector_t;
-   vector_t a(lhs.begin(), lhs.end());
-   vector_t b(rhs.begin(), rhs.end());
-   
-   // call dot function
-   double result = simd_dot(&a[0], &a[0] + a.size(), &b[0]);
-   
-   return result;
+   return simd_dot_impl(
+      REAL(lhs),
+      REAL(lhs) + lhs.size(),
+      REAL(rhs)
+   );
 }
 
 /*** R
-n <- 1024
-lhs <- rnorm(n)
-rhs <- rnorm(n)
-result <- dot(lhs, rhs)
-all.equal(result, sum(lhs * rhs))
+lhs <- rnorm(1024 * 1000)
+rhs <- rnorm(1024 * 1000)
 
-library(microbenchmark)
-lhs <- rnorm(n * 1000)
-rhs <- rnorm(n * 1000)
+all.equal(simd_dot(lhs, rhs), sum(lhs * rhs))
 microbenchmark(
-   simd = dot(lhs, rhs),
+   simd = simd_dot(lhs, rhs),
    R = sum(lhs * rhs)
 )
 */
