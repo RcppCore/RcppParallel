42c63558a4f7abb74150c33a6746b2aeb687173c
Kevin Ushey <kevinushey@gmail.com>
2018-01-05 09:13:40 -0800

    provide portable (works on Windows) configure

---
+++ R/config/scripts/variables.R
@@ -0,0 +1,22 @@
+configure <- function() {
+   
+   if (getRversion() < "3.4.0") {
+      config <- list(
+         CC            = "$(CC)",
+         CXX11         = "$(CXX1X)",
+         CXX11STD      = "$(CXX1XSTD)",
+         CXX11PICFLAGS = "$(CXX1XPICFLAGS)"
+      )
+   } else {
+      config <- list(
+         CC            = "$(CC)",
+         CXX11         = "$(CXX11)",
+         CXX11STD      = "$(CXX11STD)",
+         CXX11PICFLAGS = "$(CXX11PICFLAGS)"
+      )
+   }
+   
+   config$STDVER <- "c++11"
+   
+   config
+}
