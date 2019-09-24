5c9505f666659e882dd201ba3dd8fe0d8061d693
Kevin Ushey <kevinushey@gmail.com>
2019-09-24 10:56:46 -0700

    CRAN release 4.4.4

--- tools/config/cleanup.R
+++ tools/config/cleanup.R
@@ -1,4 +1,4 @@
 # Clean up files generated during configuration here.
 # Use 'remove_file()' to remove files generated during configuration.
-
+unlink("src/tbb/build/lib_release", recursive = TRUE)
 
