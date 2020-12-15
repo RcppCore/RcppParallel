1622cf9918a949b0310abe8a917a7d5bb9cd1636
Hamada S. Badr <hamada.s.badr@gmail.com>
2020-12-15 09:52:16 -0500

    Use tbb::this_task_arena::max_concurrency()

--- src/options.cpp
+++ src/options.cpp
@@ -11,7 +11,7 @@
 extern "C" SEXP defaultNumThreads() {
    SEXP threadsSEXP = Rf_allocVector(INTSXP, 1);
 #ifndef __TBB_tbb_stddef_H
-   INTEGER(threadsSEXP)[0] = tbb::global_control::max_allowed_parallelism;
+   INTEGER(threadsSEXP)[0] = tbb::this_task_arena::max_concurrency();
 #else
    INTEGER(threadsSEXP)[0] = tbb::task_scheduler_init::default_num_threads();
 #endif
